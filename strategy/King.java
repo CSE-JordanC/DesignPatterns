0b46a0116b07156ea680c24261a2de37a16245d0
CSE-JordanC <chavousn@email.sc.edu>
2025-08-24 22:11:25 -0400

    Finished Assignment

--- strategy/King.java
+++ strategy/King.java
@@ -1,11 +1,14 @@
 package DesignPatterns.strategy;
 
+//King class extends from Character since character is the super/parent class
 public class King extends Character{
+    //Takes name from users input and sets it as the Kings name
     public King(String name)
     {
         super(name);
     }
 
+    //Fills in the empty toString method so you can display the Kings name and symbol for it
     public String toString(){
         return "__/^^^\\__" + "\nYour character is a King named " + name;
     }
