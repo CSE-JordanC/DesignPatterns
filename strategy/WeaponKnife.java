80bd17e9083368196b75e8cc93539df1138fe883
CSE-JordanC <chavousn@email.sc.edu>
2025-08-24 21:17:23 -0400

    Code runs, but no javadoc yet.

--- strategy/WeaponKnife.java
+++ strategy/WeaponKnife.java
@@ -1,7 +1,15 @@
 package DesignPatterns.strategy;
 
-public class WeaponKnife implements WeaponBehavior{
+public class WeaponKnife extends WeaponBehavior{
     public void attack(){
-        System.out.print("-");
+        for (int i = 0; i<150; ++i){
+            Resources.clear();
+            for(int j = 0; j<i; ++j){
+                System.out.print(" ");
+            }
+            System.out.print("-|==>");
+            Resources.sleep(20);
+        }
+        System.out.println(" ");
     }
 }
