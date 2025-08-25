48bfe3e2cdbb24a84c9a060e474405b43b242008
CSE-JordanC <chavousn@email.sc.edu>
2025-08-24 22:05:29 -0400

    Documentation and output file added

--- strategy/WeaponNone.java
+++ strategy/WeaponNone.java
@@ -2,6 +2,7 @@ package DesignPatterns.strategy;
 
 public class WeaponNone extends WeaponBehavior{
     public void attack(){
+        Resources.clear();
         System.out.print("Your hands are too feedble, you faint from exhaustion.\n");
     }
 }
--- strategy/WeaponStars.java
+++ strategy/WeaponStars.java
@@ -3,7 +3,7 @@ package DesignPatterns.strategy;
 public class WeaponStars extends WeaponBehavior{
     public void attack(){
         System.out.println("");
-        
+        Resources.clear();
         for(int i = 0; i<100; ++i){
             System.out.print("*");
             Resources.sleep(30);
